b07f914146e5f574758f324442e4e43c28d7e09f
bidah <bartoindahouse@gmail.com>
2016-05-03 15:23:44 -0400

    student table

--- lib/student.rb
+++ lib/student.rb
@@ -2,6 +2,6 @@ class Student
   attr_accessor :id, :name,  :tagline, :github, :twitter, :blog_url, :image_url, :biography
   
   def self.create_table
-    DB[:conn].execute("CREATE TABLE students( id INTEGER PRIMARY ID, name TEXT, tagline TEXT, github TEXT, twitter TEXT, blog_url TEXT, image_url TEXT, biography TEXT)")
+    DB[:conn].execute("CREATE TABLE students( id INTEGER PRIMARY KEY, name TEXT, tagline TEXT, github TEXT, twitter TEXT, blog_url TEXT, image_url TEXT, biography TEXT)")
   end
 end
