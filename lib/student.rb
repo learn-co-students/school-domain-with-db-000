2b25b6323ca854a84007da993cd78e6b59a433f2
bidah <bartoindahouse@gmail.com>
2016-05-03 16:05:24 -0400

    insert done

--- lib/student.rb
+++ lib/student.rb
@@ -4,4 +4,13 @@ class Student
   def self.create_table
     DB[:conn].execute("CREATE TABLE students( id INTEGER PRIMARY KEY, name TEXT, tagline TEXT, github TEXT, twitter TEXT, blog_url TEXT, image_url TEXT, biography TEXT)")
   end
+
+  def self.drop_table
+    DB[:conn].execute("DROP TABLE students") 
+  end
+
+  def insert
+    DB[:conn].execute("INSERT INTO students (name, tagline, github, twitter, blog_url, image_url, biography) VALUES (?,?,?,?,?,?,?)", [name, tagline, github, twitter, blog_url, image_url, biography]) 
+  end
+
 end
