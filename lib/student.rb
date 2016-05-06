d206bc724895a6f2af02cb17ccac1dc8daa878be
bidah <bartoindahouse@gmail.com>
2016-05-05 21:21:44 -0400

    done

--- lib/student.rb
+++ lib/student.rb
@@ -11,6 +11,44 @@ class Student
 
   def insert
     DB[:conn].execute("INSERT INTO students (name, tagline, github, twitter, blog_url, image_url, biography) VALUES (?,?,?,?,?,?,?)", [name, tagline, github, twitter, blog_url, image_url, biography]) 
+    self.id=(DB[:conn].execute("SELECT last_insert_rowid() FROM students").flatten.first)
+  end
+ 
+  def self.new_from_db(row)
+    Student.new.tap do |s|
+      s.id        = row[0]
+      s.name      = row[1]
+      s.tagline   = row[2]
+      s.github    = row[3]
+      s.twitter   = row[4]
+      s.blog_url  = row[5]
+      s.image_url = row[6]
+      s.biography = row[7]
+    end
+  end
+  
+  def self.find_by_name(name)
+    row = DB[:conn].execute("SELECT * FROM students WHERE name = '#{name}'").flatten
+    if row.empty?
+      nil
+    else
+      student = Student.new_from_db(row)
+      student
+    end
+  end
+
+  
+  def update
+    DB[:conn].execute("UPDATE students SET name = '#{name}' WHERE id = '#{id}'") 
+  end
+
+  def save
+    row = DB[:conn].execute("SELECT * FROM students where id = '#{id}'").flatten
+    if row.empty? 
+      self.insert
+    else
+      self.update
+    end
   end
 
 end
